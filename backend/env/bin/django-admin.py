5193bbdad1ab958c60c8584619474d9f9ad89839
Allen Wang <allenwang@mbp-de-master.lan>
2020-05-28 12:34:11 -0400

    added git ignore

--- backend/env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/allenwang/Desktop/React Workspace/markov-experiment/backend/env/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
